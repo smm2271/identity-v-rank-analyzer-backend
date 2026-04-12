e571e5b28e0f7d847c58391f9e755c8bdae45f2b
smm_2271 <zalicleexu@gmail.com>
2026-04-12 17:02:58 +0000

    fix: 移除不必要的主程式啟動代碼

--- app.py
+++ app.py
@@ -130,8 +130,3 @@ app.include_router(match_router, prefix="/api/v1")
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
-
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=9999)
-    
